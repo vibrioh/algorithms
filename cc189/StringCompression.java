76f4202213fa25ae63674d1d696bbc6aa1a15941
vibrioh <vibrioh@gmail.com>
2017-07-27 00:10:17 -0400

    don't have to use StringBuilder, string can add parts.

--- cc189/StringCompression.java
+++ cc189/StringCompression.java
@@ -5,26 +5,47 @@ package cc189;
  of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the
  "compressed" string would not become smaller than the original string, your method should return
  the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+ *
  * Consider the beginning, forget the ending!!!!!
  */
 public class StringCompression {
 
   public static String stringCompressionBuilder(String str) {
-    StringBuilder strBldr = new StringBuilder();
-    strBldr.append(str.charAt(0));
+//    StringBuilder strBldr = new StringBuilder();
+//    strBldr.append(str.charAt(0));
+    String strComp = "" + str.charAt(0);
     int repeat = 1;
     for (int i = 1; i < str.length(); i++) {
       if (str.charAt(i) == str.charAt(i - 1)) {
         repeat += 1;
       } else {
-        strBldr.append(repeat);
-        strBldr.append(str.charAt(i));
+//        strBldr.append(repeat);
+//        strBldr.append(str.charAt(i));
+        strComp += "" + repeat + str.charAt(i);
         repeat = 1;
       }
     }
-    strBldr.append(repeat);
-    return (strBldr.length() < str.length()) ? strBldr.toString() : str;
+//    strBldr.append(repeat);
+    strComp += "" + repeat;
+//    return (strBldr.length() < str.length()) ? strBldr.toString() : str;
+    return (strComp.length() < str.length()) ? strComp : str;
   }
+
+  public static String compressBad(String str) {
+    String compressedString = "";
+    int countConsecutive = 0;
+    for (int i = 0; i < str.length(); i++) {
+      countConsecutive++;
+
+			/* If next character is different than current, append this char to result.*/
+      if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+        compressedString += "" + str.charAt(i) + countConsecutive;
+        countConsecutive = 0;
+      }
+    }
+    return compressedString.length() < str.length() ? compressedString : str;
+  }
+
 
   public static void main(String[] args) {
     String[] str = {"aaaaabbbbaaaabbddcddddddddd",
@@ -33,7 +54,7 @@ public class StringCompression {
         "asdfjkl;asdlfadasdfsssssssssdddd",
         "94nm0900dfdddddiek"};
     for (String s : str) {
-      System.out.println(s + ":   " + stringCompressionBuilder(s));
+      System.out.println(s + ":   " + stringCompressionBuilder(s) + " | " + compressBad(s));
     }
   }
 }
