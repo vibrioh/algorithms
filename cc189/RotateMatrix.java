a0071b2ef959b4ec93bd47bb8bc2cd749e94704b
vibrioh <vibrioh@gmail.com>
2017-07-28 19:25:00 -0400

    add book solution, consider each moving array's first and last elements

--- cc189/RotateMatrix.java
+++ cc189/RotateMatrix.java
@@ -31,6 +31,33 @@ public class RotateMatrix {
     return mtx;
   }
 
+  public static boolean rotate(int[][] matrix) {
+    if (matrix.length == 0 || matrix.length != matrix[0].length) return false; // Not a square
+    int n = matrix.length;
+
+    for (int layer = 0; layer < n / 2; layer++) {
+      int first = layer;
+      int last = n - 1 - layer;
+      for(int i = first; i < last; i++) {
+        int offset = i - first;
+        int top = matrix[first][i]; // save top
+
+        // left -> top
+        matrix[first][i] = matrix[last-offset][first];
+
+        // bottom -> left
+        matrix[last-offset][first] = matrix[last][last - offset];
+
+        // right -> bottom
+        matrix[last][last - offset] = matrix[i][last];
+
+        // top -> right
+        matrix[i][last] = top; // right <- saved top
+      }
+    }
+    return true;
+  }
+
   public static void main(String[] args) {
     final int N = 9;
     int count = 0;
@@ -66,6 +93,19 @@ public class RotateMatrix {
       }
       System.out.println();
     }
+
+    rotate(mtx);
+
+    System.out.println("--------------------------------------------");
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        System.out.printf("[%2d]", mtx[i][j]);
+      }
+      System.out.println();
+    }
+
+
   }
 
 }
