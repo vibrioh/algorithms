92bdf837da3fa01c48c131767f833c56d7d9bd9f
vibrioh <vibrioh@gmail.com>
2017-07-29 00:52:15 -0400

    equal len, isSubString(str2, str1 + str1)

--- cc189/StringRotation.java
+++ cc189/StringRotation.java
@@ -3,7 +3,7 @@ package cc189;
 public class StringRotation {
 
   // is str1 is a substring of str2?
-  public static boolean isSubstring(String str1, String str2) {
+  public static boolean isSubString(String str1, String str2) {
     if (str1.length() > str2.length()) {
       return false;
     }
@@ -22,24 +22,78 @@ public class StringRotation {
     return false;
   }
 
+  // just concatenate str1
+  public static boolean isStringRotation(String str1, String str2) {
+//    String str1str1 = str1 + str1;
+//    System.out.println(str3);
+    return isSubString(str2, str1 + str1);
+  }
+
+//  public static String stringSort(String str) {    // Arrays.sort() void!!
+//    char[] charr = str.toCharArray();
+//    Arrays.sort(charr);
+//    return new String(charr);
+//  }
+
+  // rotation changed the order, so sort would work
+  /**
+   * only sort won't work, lost original order info
+   */
+//  public static boolean isStringRotation(String str1, String str2) {
+//    if (str1.length() != str2.length()) {
+//      return false;
+//    }
+//    if (isSubString(stringSort(str1), stringSort(str2))) {
+//      return true;
+//    }
+//    return false;
+//  }
+
+  public static boolean isStringRevers(String str1, String str2) {    // this is not the problem asked
+    if (str1.length() != str2.length()) {
+      return false;
+    }
+    for (int i = 0; i < str1.length(); i++) {
+//      System.out.printf("[%s][%s] \n", str1.charAt(i), str2.charAt(str1.length() - 1 - i));
+      if (str1.charAt(i) != str2.charAt(str1.length() - 1 - i)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   public static void main(String[] args) {
-    String[] str1s = {"",
-    " ",
-    "love",
-    "I",
-    "I ",
-    "e d",
-    "!",
-        "doggy!",
-        "I love doggy!",
-    "doggy! ",
-    "I love doggy! "};
+    String[] str1s = {"", " ", "love", "I", "I ", "e d", "!", "doggy!", "I love doggy!", "doggy! ", "I love doggy! "};
     String str2 = "I love doggy!";
     for (String str1 : str1s) {
-      if (isSubstring(str1, str2)) {
+      if (isSubString(str1, str2)) {
         System.out.printf("{%s} is a subset of {%s}\n", str1, str2);
       } else {
         System.out.printf("{%s} is NOT a subset of {%s}\n", str1, str2);
+      }
+    }
+
+    System.out.println("------------------------------------------------------");
+
+    String[] str3s = {"elttobretaw", "elttobretaw ", "elttobreta", "waterbottle", "", " ", "elttobretaw"};
+    String str4 = "waterbottle";
+    for (String str3 : str3s) {
+      if (isStringRevers(str3, str4)) {
+        System.out.printf("{%s} is a revers of {%s}\n", str3, str4);
+      } else {
+        System.out.printf("{%s} is NOT a revers of {%s}\n", str3, str4);
+      }
+    }
+
+    System.out.println("------------------------------------------------------");
+
+    String[] str5s = {"elttobretaw", "elttobretaw ", "elttobreta", "waterbottle", "", " ", "elttobretaw", "rbottlewate"};
+    String str6 = "waterbottle";
+    for (String str5 : str5s) {
+      if (isStringRotation(str5, str6)) {
+        System.out.printf("{%s} is a rotation of {%s}\n", str5, str6);
+      } else {
+        System.out.printf("{%s} is NOT a rotation of {%s}\n", str5, str6);
       }
     }
   }
