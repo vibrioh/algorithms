dd6fb6e8664fb71d579661e3953cd044b4835ead
vibrioh <vibrioh@gmail.com>
2017-08-01 18:17:23 -0400

    stack two pointers approach

--- cc189/LinkedListPalindrome.java
+++ cc189/LinkedListPalindrome.java
@@ -2,6 +2,8 @@ package cc189;
 
 import static cc189.ReversedLinkedList.newLinkedListReversed;
 
+import java.util.Stack;
+
 public class LinkedListPalindrome {
 
   static boolean isLinkedListPalinedrome(Node head) {
@@ -18,21 +20,60 @@ public class LinkedListPalindrome {
     return true;
   }
 
+  static boolean isLinkedListPalinedromeStack(Node head) {
+    if (head == null) {
+      return false;
+    } else if (head.next == null) {
+      return true;
+    }
+    Node p1 = head;
+    Node p2 = head;
+    Stack<Integer> stack = new Stack<>();
+    stack.push(head.data);
+    while (p2 != null && p2.next != null) {    // need to check p2.next != null ,if you are using p2.next.next, or null pointer
+      stack.push(p1.data);
+//      System.out.println("\n" +p1.data);
+      p1 = p1.next;
+      p2 = p2.next.next;
+    }
+    if (p2.next == null) {    // has odd number of elements, so skip the middle element
+      p1 = p1.next;
+    }
+    while (p1 != null) {
+      if (stack.pop() != p1.data) {    // why do not need intValue()?
+        return false;
+      }
+      p1 = p1.next;
+    }
+    return true;
+  }
+
+
+
+
+
+
+
+
+
+
   public static void main(String[] args) {
     Node head = new Node(1);
     head.next = new Node(2);
     head.next.next = new Node(3);
     head.next.next.next = new Node(4);
-    head.next.next.next.next = new Node(3);
-    head.next.next.next.next.next = new Node(2);
-    head.next.next.next.next.next.next = new Node(1);
+    head.next.next.next.next = new Node(5);
+    head.next.next.next.next.next = new Node(4);
+    head.next.next.next.next.next.next = new Node(3);
+    head.next.next.next.next.next.next.next = new Node(2);
+    head.next.next.next.next.next.next.next.next = new Node(1);
 
     Node iter = head;
     while (iter != null) {
       System.out.print(iter.data + "  ");
       iter = iter.next;
     }
-    boolean newHead = isLinkedListPalinedrome(head);
+    boolean newHead = isLinkedListPalinedromeStack(head);
     System.out.println();
 
     System.out.print(newHead);
