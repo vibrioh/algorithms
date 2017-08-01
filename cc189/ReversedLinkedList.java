a0f0c92ab14fff79ffcbf1f2611f28909eb1c36e
vibrioh <vibrioh@gmail.com>
2017-08-01 16:22:43 -0400

    reference switch

---
+++ cc189/ReversedLinkedList.java
@@ -0,0 +1,51 @@
+package cc189;
+
+public class ReversedLinkedList {
+  static Node newLinkedListReversed(Node head) {
+    Node rehead = null;
+    Node curr = null;
+    while (head != null) {
+      rehead = new Node(head.data);    // this is new created.
+      rehead.next =curr;
+      curr = rehead;
+      head = head.next;
+    }
+    return rehead;
+  }
+
+  static Node linkedListReversed(Node head) {
+    Node curr = null;
+    Node prev = null;
+    while (head != null) {
+      prev = head.next;    // reference switch
+      head.next = curr;
+      curr = head;
+      head = prev;
+    }
+    head = curr;
+    return head;
+  }
+
+
+  public static void main(String[] args) {
+    Node head = new Node(3);
+    head.next = new Node(5);
+    head.next.next = new Node(8);
+    head.next.next.next = new Node(5);
+    head.next.next.next.next = new Node(10);
+    head.next.next.next.next.next = new Node(2);
+    head.next.next.next.next.next.next = new Node(1);
+
+    Node iter = head;
+    while (iter != null) {
+      System.out.print(iter.data + "  ");
+      iter = iter.next;
+    }
+    Node newHead = linkedListReversed(head);
+    System.out.println();
+    while (newHead != null) {
+      System.out.print(newHead.data + "  ");
+      newHead = newHead.next;
+    }
+  }
+}
