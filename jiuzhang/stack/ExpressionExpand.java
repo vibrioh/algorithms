7a770161e98d56802981dedba49fba17581351c2
vibrioh <vibrioh@gmail.com>
2017-08-25 22:14:54 -0400

    char to String

---
+++ jiuzhang/stack/ExpressionExpand.java
@@ -0,0 +1,12 @@
+package jiuzhang.stack;
+
+public class ExpressionExpand {
+
+  public static void main(String[] args){
+    char a = '0';
+    char b = '9';
+    System.out.println(" print 'a' get " + a + "\n print (int) a = " + (int) a);
+    System.out.println(" print 'b' get " + b + "\n print (int) b = " + (int) b);
+    System.out.println("We need get int of 'b' by 'b' - '0': " + (b - '0'));
+  }
+}
