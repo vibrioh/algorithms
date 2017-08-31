5d835b72e3021e13937669e3387b1043dfd200ca
vibrioh <vibrioh@gmail.com>
2017-08-31 16:08:53 -0400

    divide and conquer

---
+++ leetcode/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import jiuzhang.binarytree.TreeNode;
+
+public class MaximumDepthOfBinaryTree {
+  /**
+   * Definition for a binary tree node.
+   * public class TreeNode {
+   *     int val;
+   *     TreeNode left;
+   *     TreeNode right;
+   *     TreeNode(int x) { val = x; }
+   * }
+   */
+  class Solution {
+    public int maxDepth(TreeNode root) {
+      if (root == null) {
+        return 0;
+      }
+
+      return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+  }
+}
