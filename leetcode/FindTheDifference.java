af31472791ed913ffe940dc2520610f72f888d5b
vibrioh <vibrioh@gmail.com>
2017-09-05 22:08:44 -0400

    Arrays.sort(char[]) int or bit sum(t) - sum(s)

--- leetcode/FindTheDifference.java
+++ leetcode/FindTheDifference.java
@@ -18,4 +18,23 @@ public class FindTheDifference {
     }
   }
 
+  public char findTheDifference2(String s, String t) {
+    int charCode = t.charAt(s.length());
+    // Iterate through both strings and char codes
+    for (int i = 0; i < s.length(); ++i) {
+      charCode -= (int)s.charAt(i);
+      charCode += (int)t.charAt(i);
+    }
+    return (char)charCode;
+  }
+
+  public char findTheDifference3(String s, String t) {
+    int n = t.length();
+    char c = t.charAt(n - 1);
+    for (int i = 0; i < n - 1; ++i) {
+      c ^= s.charAt(i);
+      c ^= t.charAt(i);
+    }
+    return c;
+  }
 }
