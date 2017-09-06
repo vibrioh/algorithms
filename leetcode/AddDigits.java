cbcdb6b213e53f6b51e24e37b76cdf418f13d110
vibrioh <vibrioh@gmail.com>
2017-09-05 21:50:26 -0400

    math, digital root

---
+++ leetcode/AddDigits.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/**
+ * https://en.wikipedia.org/wiki/Digital_root
+ *
+ */
+
+public class AddDigits {
+  class Solution {
+    public int addDigits(int num) {
+      return (num - 1)%9 + 1;
+    }
+  }
+}
