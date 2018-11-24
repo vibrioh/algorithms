b6f81102a75e1e80885cd083d1b20ff0c73ee6a1
vibrioh <vibrioh@gmail.com>
2018-11-23 22:42:05 -0500

    isValid Java Basics

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -672,5 +672,23 @@ public class Solutions {
         }
     }
 
+    public boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                if (stack.isEmpty() || map.get(s.charAt(i)) != stack.pop()) {
+                    return false;
+                }
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+        return stack.isEmpty();
+    }
+
 
 }
