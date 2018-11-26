fbaf377f9c3b7f7dd4510f2aad0f28b04c3781a7
Jun Hu <junhu@Juns-MacBook-Pro.local>
2018-11-26 17:02:07 -0500

    move a smaller is wrong

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -798,5 +798,30 @@ public class Solutions {
         return dp[n];
     }
 
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        int max = sum;
+        while (start < end) {
+            System.out.println(sum);
+            if (nums[start] < nums[end]) {
+                sum -= nums[start];
+                start++;
+            } else {
+                sum -= nums[end];
+                end--;
+            }
+            max = Math.max(sum, max);
+        }
+        return max;
+    }
+
 
 }
