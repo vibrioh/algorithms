e106ee8778bea342b3e7721808cb43ebca7851bb
Jun Hu <junhu@Juns-MacBook-Pro.local>
2018-11-26 16:00:51 -0500

    corner case at start

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -784,5 +784,19 @@ public class Solutions {
         return Math.min(dp[n - 1], dp[n - 2]);
     }
 
+    public int climbStairs(int n) {
+        // current solutions at step n
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
 
 }
