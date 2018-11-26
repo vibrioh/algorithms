1a27a034e5b16330d6f89d3d865f4c663157e52d
Jun Hu <junhu@Juns-MacBook-Pro.local>
2018-11-26 14:10:00 -0500

    min max update status

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -751,5 +751,15 @@ public class Solutions {
         return min;
     }
 
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for (int i = 0; i < prices.length; i++) {
+            max = Math.max(max, prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+        return max;
+    }
+
 
 }
