c6c05ca7d45a2ed411d842601aad9083763e9400
Jun Hu <junhu@Juns-MacBook-Pro.local>
2018-12-07 10:50:50 -0500

    binary search

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -946,4 +946,32 @@ public class Solutions {
         System.out.println(Arrays.toString(nums));
     }
 
+    public int searchInsert(int[] nums, int target) {
+        if (target < nums[0]) {
+            return 0;
+        }
+        if (target > nums[nums.length - 1]) {
+            return nums.length;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target < nums[mid]) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        if (target == nums[start]) {
+            return start;
+        } else if (target == nums[end]) {
+            return end;
+        } else {
+            return start + 1;
+        }
+    }
+
 }
