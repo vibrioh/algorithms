cb99d20ef6faa48dcfca06c88277f4586dd54172
vibrioh <vibrioh@gmail.com>
2018-12-26 03:27:08 -0500

    merge intervals sort by extra Arrays

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -1269,4 +1269,45 @@ public class Solutions {
         return canJumpFromPosition(0, nums);
     }
 
+    public class Interval {
+        int start;
+        int end;
+
+        Interval() {
+            start = 0;
+            end = 0;
+        }
+
+        Interval(int s, int e) {
+            start = s;
+            end = e;
+        }
+    }
+
+    public List<Interval> merge(List<Interval> intervals) {
+        List<Interval> res = new ArrayList<>();
+        if (intervals == null || intervals.size() == 0) {
+            return res;
+        }
+        int len = intervals.size();
+        int[] starts = new int[len];
+        int[] ends = new int[len];
+        for (int i = 0; i < len; i++) {
+            starts[i] = intervals.get(i).start;
+            ends[i] = intervals.get(i).end;
+        }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        int currStart = starts[0];
+        for (int i = 0; i < len; i++) {
+            if (i == len - 1) {
+                res.add(new Interval(currStart, ends[i]));
+            } else if (ends[i] < starts[i + 1]) {
+                res.add(new Interval(currStart, ends[i]));
+                currStart = starts[i + 1];
+            }
+        }
+        return res;
+    }
+
 }
