88fe2b8a0e210f4635899a8b2e58775cb9386de5
vibrioh <vibrioh@gmail.com>
2019-01-02 22:04:59 -0500

    all increased are profit

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -1427,4 +1427,14 @@ public class Solutions {
         return res;
     }
 
+    public int maxProfitII(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
+
 }
