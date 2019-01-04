59680eacacb790be00d94227196451bc86872aa8
vibrioh <vibrioh@gmail.com>
2019-01-04 15:19:42 -0500

    use equals to check Integer

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -1543,4 +1543,38 @@ public class Solutions {
         return false;
     }
 
+    class MinStack {
+        private Stack<Integer> stack;
+        private Stack<Integer> minStack;
+    
+        /** initialize your data structure here. */
+        public MinStack() {
+            stack = new Stack<>();
+            minStack = new Stack<>();
+        }
+        
+        public void push(int x) {
+            if (minStack.isEmpty() || x <= minStack.peek()) {
+                minStack.push(x);
+            }
+            stack.push(x);
+            return;
+        }
+        
+        public void pop() {
+            if (stack.pop().equals(minStack.peek())) {
+                minStack.pop();
+            }
+            return;
+        }
+        
+        public int top() {
+            return stack.peek();
+        }
+        
+        public int getMin() {
+            return minStack.peek();
+        }
+    }
+
 }
