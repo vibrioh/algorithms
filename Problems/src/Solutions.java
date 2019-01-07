50f133c7e8d4184320b17518cc9e4667e2f3aa12
vibrioh <vibrioh@gmail.com>
2019-01-06 23:07:06 -0500

    dp for solution num, n -1 m - 1

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -1673,4 +1673,21 @@ public class Solutions {
         return;
     }
 
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (r > 0 && c > 0) {
+                    dp[r][c] = dp[r - 1][c] + dp[r][c - 1];
+                } else if (r > 0) {
+                    dp[r][c] = dp[r - 1][c];
+                } else if (c > 0) {
+                    dp[r][c] = dp[r][c - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
 }
