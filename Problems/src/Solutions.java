f99b43ec6783e9a32e3bd3dd6126f9449e189719
vibrioh <vibrioh@gmail.com>
2019-01-06 22:44:52 -0500

    add from end of arrays

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -1655,4 +1655,22 @@ public class Solutions {
         dfsSubsets(nums, result, results, idx + 1);
     }
 
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = m + n - 1; i >= 0; i--) {
+            if (m > 0 && n > 0) {
+                if (nums2[n - 1] >= nums1[m - 1]) {
+                    nums1[i] = nums2[n - 1];
+                    n--;
+                } else {
+                    nums1[i] = nums1[m - 1];
+                    m--;
+                }
+            } else if (m == 0) {
+                nums1[i] = nums2[n - 1];
+                n--;
+            }
+        }
+        return;
+    }
+
 }
