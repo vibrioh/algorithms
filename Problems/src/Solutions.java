2a74811f49385a7d20330916a9b8fa4553f9d687
vibrioh <vibrioh@gmail.com>
2019-01-07 16:59:56 -0500

    use heap/PriorityQueue for k size()

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -1691,8 +1691,14 @@ public class Solutions {
     }
 
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length - k];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int n : nums) {
+            pq.add(n);
+            if (pq.size() == k + 1) {
+                pq.poll();
+            }
+        }
+        return pq.poll();
     }
 
 }
