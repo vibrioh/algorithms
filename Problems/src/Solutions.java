cfeafccd0cd6dcbbf33170578cd34b82b7da2b7c
vibrioh <vibrioh@gmail.com>
2019-01-10 15:49:01 -0500

    dp think the coner, and take care of coninue

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -1903,4 +1903,31 @@ public class Solutions {
         return null;
     }
 
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0) {
+            return 0;
+        }
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (r == 0 && c == 0) {
+                    dp[r][c] = 1;
+                }
+                if (obstacleGrid[r][c] == 1) {
+                    dp[r][c] = 0;
+                    continue;
+                }
+                if (r - 1 >= 0) {
+                    dp[r][c] += dp[r - 1][c];
+                }
+                if (c - 1 >= 0) {
+                    dp[r][c] += dp[r][c - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
 }
