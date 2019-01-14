f4125e8a747df6b98990fc7bed40091b7e59b8b8
vibrioh <vibrioh@gmail.com>
2019-01-14 00:00:23 -0500

    two pointer sum

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -1930,4 +1930,23 @@ public class Solutions {
         return dp[m - 1][n - 1];
     }
 
+    public int minSubArrayLen(int s, int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int left = 0;
+        int sum = 0;
+        int res = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            while (sum >= s) {
+                res = Math.min(res, i + 1 - left);
+                sum -= nums[left++];
+            }
+        }
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+
 }
