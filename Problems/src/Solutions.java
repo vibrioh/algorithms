eb919f2cb732a88548c913f2ba7b5f17115af04f
vibrioh <vibrioh@gmail.com>
2019-01-22 11:03:00 -0500

    set curr bar, move small side, update bar with larger bar

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -2155,4 +2155,34 @@ public class Solutions {
         }
         return res;
     }
+
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int res = 0;
+        int left = 0;
+        int right = height.length - 1;
+        int curr = 0;
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                if (curr < height[left]) {
+                    curr = height[left];
+                }
+                if (curr > height[left]) {
+                    res += curr - height[left];
+                }
+                left++;
+            } else {
+                if (curr < height[right]) {
+                    curr = height[right];
+                }
+                if (curr > height[right]) {
+                    res += curr - height[right];
+                }
+                right--;
+            }
+        }
+        return res;
+    }
 }
