ceab199d2d6eeed54544f93d982ad171b07fe68a
vibrioh <vibrioh@gmail.com>
2019-02-12 16:01:39 -0500

    in a while loop, you don't need every condition, you can move index one loop by one loop, be clear. Primary type need its Class to invoke class method

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -2561,4 +2561,22 @@ public class Solutions {
         return res;
     }
 
+    public boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            if (!Character.isLetterOrDigit(s.charAt(start))) {
+                start++;
+            } else if (!Character.isLetterOrDigit(s.charAt(end))) {
+                end--;
+            } else if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+                return false;
+            } else {
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+
 }
