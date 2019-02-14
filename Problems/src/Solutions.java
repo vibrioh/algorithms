bb12f43817f090a10ea84730d4d5e93e8b37cc0e
vibrioh <vibrioh@gmail.com>
2019-02-14 12:05:19 -0500

    left right BST

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -2659,4 +2659,19 @@ public class Solutions {
         return res;
     }
 
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) {
+            return null;
+        }
+        root.left = trimBST(root.left, L, R);
+        root.right = trimBST(root.right, L, R);
+        if (root.val >= L && root.val <= R) {
+            return root;
+        } else if (root.val < L) {
+            return root.right;
+        } else {
+            return root.left;
+        }
+    }
+
 }
