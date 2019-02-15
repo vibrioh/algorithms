2e97f771985c4a8a1ed6f7d8fe0bd9e3ba63c467
vibrioh <vibrioh@gmail.com>
2019-02-15 15:20:10 -0500

    projection for overlap

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -2877,4 +2877,11 @@ public class Solutions {
         return Math.max(left + 1, right + 1);
     }
 
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        if (rec1[0] >= rec2[2] || rec2[0] >= rec1[2] || rec1[1] >= rec2[3] || rec2[1] >= rec1[3]) {
+            return false;
+        }
+        return true;
+    }
+
 }
