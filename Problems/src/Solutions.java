b0590170977ba87dc68c634ec38d501e2c1ef2ea
vibrioh <vibrioh@gmail.com>
2019-02-23 23:28:53 -0500

    idear of prefix sum

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -3084,4 +3084,21 @@ public class Solutions {
         return;
     }
 
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int[] preSum = new int[n + 1];
+        preSum[0] = 0;
+        for (int i = 1; i < n + 1; i++) {
+            preSum[i] = preSum[i - 1] + nums[i - 1];
+            // System.out.println(preSum[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (preSum[i] == preSum[n] - preSum[i + 1]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
