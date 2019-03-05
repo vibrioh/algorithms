bb9532c7e55553d81b1895c300c122d633e31f2d
vibrioh <vibrioh@gmail.com>
2019-03-05 16:46:38 -0500

    for int[] just store idx, because you can retieve [idx] for value

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -3238,4 +3238,17 @@ public class Solutions {
         return res;
     }
 
+
+    public int[] dailyTemperatures(int[] T) {
+        int[] res = new int[T.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < T.length; i++) {
+            while (!stack.isEmpty() && T[stack.peek()] < T[i]) {
+                res[stack.peek()] = i - stack.pop();
+            }
+            stack.add(i);
+        }
+        return res;
+    }
+
 }
