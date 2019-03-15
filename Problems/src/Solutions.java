6fbd94745b87898a1cefb1a189005bff60e6ec1b
vibrioh <vibrioh@gmail.com>
2019-03-15 14:17:42 -0400

    reverse add and reverse

--- Problems/src/Solutions.java
+++ Problems/src/Solutions.java
@@ -3660,6 +3660,48 @@ public class Solutions {
 
     }
 
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        l1 = reverseLinkedList(l1);
+        l2 = reverseLinkedList(l2);
+        int carry = 0;
+        ListNode l = null;
+        ListNode dummy = new ListNode(0);
+        while (l1 != null || l2 != null) {
+            int v1 = l1 == null ? 0 : l1.val;
+            int v2 = l2 == null ? 0 : l2.val;
+            int pos = (v1 + v2 + carry) % 10;
+            carry = (v1 + v2 + carry) / 10;
+            if (l == null) {
+                l = new ListNode(pos);
+                dummy.next = l;
+            } else {
+                l.next = new ListNode(pos);
+                l = l.next;
+            }
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry != 0) {
+            l.next = new ListNode(carry);
+        }
+        return reverseLinkedList(dummy.next);
+    }
+
+    private ListNode reverseLinkedList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode tmp = head.next;
+            head.next = prev;
+            prev = head;
+            head = tmp;
+        }
+        return prev;
+    }
+
     public int maxProfit(int[] prices, int fee) {
         if (prices == null || prices.length == 0) {
             return 0;
