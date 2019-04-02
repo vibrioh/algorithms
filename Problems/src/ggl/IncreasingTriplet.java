15bec34fefd7208e630ccf82dafc4f7194421ce9
vibrioh <vibrioh@gmail.com>
2019-04-02 12:46:37 -0400

    update small big

--- Problems/src/ggl/IncreasingTriplet.java
+++ Problems/src/ggl/IncreasingTriplet.java
@@ -1,4 +1,21 @@
 package ggl;
 
+/*
+只有同时大于前面两个数才是连续三个升序，最前面的keep最小，中间的keep当前最大
+ */
 public class IncreasingTriplet {
+    public boolean increasingTriplet(int[] nums) {
+        // start with two largest values, as soon as we find a number bigger than both, while both have been updated, return true.
+        int small = Integer.MAX_VALUE, big = Integer.MAX_VALUE;
+        for (int n : nums) {
+            if (n <= small) {
+                small = n;
+            } // update small if n is smaller than both
+            else if (n <= big) {
+                big = n;
+            } // update big only if greater than small but smaller than big
+            else return true; // return if you find a number bigger than both
+        }
+        return false;
+    }
 }
