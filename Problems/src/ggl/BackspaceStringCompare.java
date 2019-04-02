04afbd2e65ef77af9646981e702caff25b77c331
vibrioh <vibrioh@gmail.com>
2019-04-02 12:14:55 -0400

    Stack

---
+++ Problems/src/ggl/BackspaceStringCompare.java
@@ -0,0 +1,25 @@
+package ggl;
+
+import java.util.Stack;
+
+/*
+Stack
+ */
+public class BackspaceStringCompare {
+
+    public boolean backspaceCompare(String S, String T) {
+        return build(S).equals(build(T));
+    }
+
+    public String build(String S) {
+        Stack<Character> ans = new Stack();
+        for (char c : S.toCharArray()) {
+            if (c != '#')
+                ans.push(c);
+            else if (!ans.empty())
+                ans.pop();
+        }
+        return String.valueOf(ans);
+    }
+}
+
