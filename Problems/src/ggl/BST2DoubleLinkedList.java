d7c2601193c8d9a57a0254ee32d05c42f0d7f1ad
vibrioh <vibrioh@gmail.com>
2019-04-02 23:10:40 -0400

    globle virable to record the end, bst to doublelinkedlist

---
+++ Problems/src/ggl/BST2DoubleLinkedList.java
@@ -0,0 +1,52 @@
+package ggl;
+
+import org.w3c.dom.Node;
+
+/*
+step1: inorder tranversal by recursion to connect the original BST
+step2: connect the head and tail to make it circular
+
+Tips: Using dummy node to handle corner case
+ */
+
+
+public class BST2DoubleLinkedList {
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node() {
+        }
+
+        public Node(int _val, Node _left, Node _right) {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+    }
+
+    // actually the end of the List
+    Node prev = null;
+    public Node treeToDoublyList(Node root) {
+        if (root == null) return null;
+        // start of List
+        Node dummy = new Node(0, null, null);
+        // end of List
+        prev = dummy;
+        helper(root);
+        //connect head and tail
+        prev.right = dummy.right;
+        dummy.right.left = prev;
+        return dummy.right;
+    }
+
+    private void helper (Node cur) {
+        if (cur == null) return;
+        helper(cur.left);
+        prev.right = cur;
+        cur.left = prev;
+        prev = cur;
+        helper(cur.right);
+    }
+}
