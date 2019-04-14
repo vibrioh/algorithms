ec6497f9ccbce0c7c722caa99d2c05ac432a0551
vibrioh <vibrioh@gmail.com>
2019-04-14 00:08:52 -0400

    bs

--- Problems/src/ggl/FindKthLargest.java
+++ Problems/src/ggl/FindKthLargest.java
@@ -300,4 +300,43 @@ Finally if there is only one number, from the above solution, we haven't add the
 
         return sb.length() == 0 ? "/" : sb.toString();
     }
+
+    public int[] searchRange(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        int left = -1;
+        int right = -1;
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+            if (nums[mid] >= target) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        if (nums[start] == target) {
+            left = start;
+        } else if (nums[end] == target) {
+            left = end;
+        }
+        start = 0;
+        end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+            if (nums[mid] <= target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (nums[end] == target) {
+            right = end;
+        } else if (nums[start] == target) {
+            right = start;
+        }
+        return new int[]{left, right};
+    }
 }
