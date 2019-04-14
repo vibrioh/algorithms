472dfe43e98596ca8583eb5f6ab72878271f8314
vibrioh <vibrioh@gmail.com>
2019-04-14 12:48:09 -0400

    dp

--- Problems/src/amz/Solutions.java
+++ Problems/src/amz/Solutions.java
@@ -210,5 +210,18 @@ public class Solutions {
         return prev;
     }
 
-
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> words = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int end = 1; end <= s.length(); end++) {
+            for (int start = 0; start < end; start++) {
+                if (dp[start] && words.contains(s.substring(start, end))) {
+                    dp[end] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
 }
