55bddd9cb6fbddfcffa4d19219559c1f4758b8c5
vibrioh <vibrioh@gmail.com>
2019-04-13 23:33:42 -0400

    compare String use equals()

--- Problems/src/ggl/FindKthLargest.java
+++ Problems/src/ggl/FindKthLargest.java
@@ -3,6 +3,7 @@ package ggl;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Random;
+import java.util.Stack;
 
 public class FindKthLargest {
     // nlogn 1
@@ -218,5 +219,85 @@ public class FindKthLargest {
             result += three(rest);
         }
         return result;
+    }
+
+
+    /*
+    Simple iterative solution by identifying characters one by one. One important thing is that the input is valid, which means the parentheses are always paired and in order.
+Only 5 possible input we need to pay attention:
+
+digit: it should be one digit from the current number
+'+': number is over, we can add the previous number and start a new number
+'-': same as above
+'(': push the previous result and the sign into the stack, set result to 0, just calculate the new result within the parenthesis.
+')': pop out the top two numbers from stack, first one is the sign before this pair of parenthesis, second is the temporary result before this pair of parenthesis. We add them together.
+Finally if there is only one number, from the above solution, we haven't add the number to the result, so we do a check see if the number is zero.
+     */
+
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int result = 0;
+        int number = 0;
+        int sign = 1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                number = 10 * number + (int) (c - '0');
+            } else if (c == '+') {
+                result += sign * number;
+                number = 0;
+                sign = 1;
+            } else if (c == '-') {
+                result += sign * number;
+                number = 0;
+                sign = -1;
+            } else if (c == '(') {
+                //we push the result first, then sign;
+                stack.push(result);
+                stack.push(sign);
+                //reset the sign and result for the value in the parenthesis
+                sign = 1;
+                result = 0;
+            } else if (c == ')') {
+                result += sign * number;
+                number = 0;
+                result *= stack.pop();    //stack.pop() is the sign before the parenthesis
+                result += stack.pop();   //stack.pop() now is the result calculated before the parenthesis
+
+            }
+        }
+        if (number != 0) result += sign * number;
+        return result;
+    }
+
+
+    public String simplifyPath(String path) {
+        String[] list = path.split("/");
+        Stack<String> sk = new Stack<>();
+        // int n = 1;
+        for (String s : list) {
+            if (s == null) {
+                continue;
+            }
+            // System.out.println(n++ + " {" + s + "}");
+            if (s.equals("..")) {
+                if (sk.isEmpty()) {
+                    continue;
+                }
+                sk.pop();
+            } else if (!s.equals(".") && !s.equals("")) {
+                sk.push(s);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!sk.isEmpty()) {
+            String curr = sk.pop();
+            sb.insert(0, curr);
+            sb.insert(0, "/");
+
+            // System.out.println(n++ + " " + curr);
+        }
+
+        return sb.length() == 0 ? "/" : sb.toString();
     }
 }
