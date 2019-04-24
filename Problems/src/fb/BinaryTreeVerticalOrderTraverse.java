e1aaaf353803465dd945bb70acce40ad00608261
vibrioh <vibrioh@gmail.com>
2019-04-24 15:37:14 -0400

    dfs seems not working for vertical level

--- Problems/src/fb/BinaryTreeVerticalOrderTraverse.java
+++ Problems/src/fb/BinaryTreeVerticalOrderTraverse.java
@@ -2,42 +2,25 @@ package fb;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class BinaryTreeVerticalOrderTraverse {
+    // DFS seems not working because of the order to add into the same list of vertical level
     public List<List<Integer>> verticalOrder(TreeNode root) {
-        TreeNode head = root;
-        int l = 0;
-        int r = 0;
-
-        // Can not use this method to find the width of the Tree！！！！！！！
-        while (head != null) {
-            l++;
-            head = head.left == null ? head.right : head.left;
-        }
-        head = root;
-        while (head != null) {
-            r++;
-            head = head.right == null ? head.left : head.right;
-        }
-        int len = l + r - 1;
-
-
-
-        List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < len; i++) {
-            res.add(new ArrayList<Integer>());
-        }
-        traverse(root, l - 1, res);
-        return res;
+        Map<Integer, List<Integer>> mp = new TreeMap<>();
+        traverse(root, 0, mp);
+        return new ArrayList<>(mp.values());
     }
 
-    private void traverse(TreeNode root, int idx, List<List<Integer>> res) {
+    private void traverse(TreeNode root, int idx, Map<Integer, List<Integer>> mp) {
         if (root == null) {
             return;
         }
-        res.get(idx).add(root.val);
-        traverse(root.left, idx - 1, res);
-        traverse(root.right, idx + 1, res);
+        mp.putIfAbsent(idx, new ArrayList<Integer>());
+        mp.get(idx).add(root.val);
+        traverse(root.left, idx - 1, mp);
+        traverse(root.right, idx + 1, mp);
     }
 
     class TreeNode {
