4bdf79d0e0d841c16cadded04c2da0340560d28a
vibrioh <vibrioh@gmail.com>
2019-04-25 15:55:03 -0400

    stack for functions recurrsive

---
+++ Problems/src/fb/ExclusiveTimeOfFuctions.java
@@ -0,0 +1,30 @@
+package fb;
+
+import java.util.List;
+import java.util.Stack;
+
+public class ExclusiveTimeOfFuctions {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        Stack<Integer> stack = new Stack<>();
+        int[] res = new int[n];
+        String[] s = logs.get(0).split(":");
+        stack.push(Integer.parseInt(s[0]));
+        int i = 1, prev = Integer.parseInt(s[2]);
+        while (i < logs.size()) {
+            s = logs.get(i).split(":");
+            if (s[1].equals("start")) {
+                if (!stack.isEmpty())
+                    res[stack.peek()] += Integer.parseInt(s[2]) - prev;
+                stack.push(Integer.parseInt(s[0]));
+                prev = Integer.parseInt(s[2]);
+            } else {
+                // be care of + 1 for end!
+                res[stack.peek()] += Integer.parseInt(s[2]) - prev + 1;
+                stack.pop();
+                prev = Integer.parseInt(s[2]) + 1;
+            }
+            i++;
+        }
+        return res;
+    }
+}
