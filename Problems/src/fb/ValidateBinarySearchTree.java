67e2d4f73918bff7c6513e517f13813c9c05f4c3
vibrioh <vibrioh@gmail.com>
2019-04-26 00:12:03 -0400

    always think of min max at each node, but min max can be use object to check null

--- Problems/src/fb/ValidateBinarySearchTree.java
+++ Problems/src/fb/ValidateBinarySearchTree.java
@@ -7,26 +7,23 @@ public class ValidateBinarySearchTree {
         if (root == null) {
             return true;
         }
-        if (root.left != null && root.left.val >= root.val) {
-            return false;
-        }
-        if (root.right != null && root.right.val <= root.val) {
-            return false;
-        }
-        return isValidBSTdfs(root.left, Long.MIN_VALUE, root.val) && isValidBSTdfs(root.right, root.val, Long.MAX_VALUE);
+        return isValidBSTdfs(root, null, null);
 
     }
 
-    private boolean isValidBSTdfs(TreeNode root, long min, long max) {
+    private boolean isValidBSTdfs(TreeNode root, Integer min, Integer max) {
         if (root == null) {
             return true;
         }
-        System.out.println(root.val);
-        if (root.left != null && (root.left.val >= max || root.left.val <= min || root.left.val >= root.val)) {
-            return false;
+        if (min != null) {
+            if (root.val <= min) {
+                return false;
+            }
         }
-        if (root.right != null && (root.right.val >= max || root.right.val <= min || root.right.val <= root.val)) {
-            return false;
+        if (max != null) {
+            if (root.val >= max) {
+                return false;
+            }
         }
         return isValidBSTdfs(root.left, min, root.val) && isValidBSTdfs(root.right, root.val, max);
     }
