5d35ad1dc6ae50bed39f98194c0160093de551d5
vibrioh <vibrioh@gmail.com>
2019-04-29 11:51:32 -0400

    easy

---
+++ Problems/src/fb/RansomNote.java
@@ -0,0 +1,17 @@
+package fb;
+
+public class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] counts = new int[26];
+        for (char c : magazine.toCharArray()) {
+            counts[c - 'a']++;
+        }
+        for (char c : ransomNote.toCharArray()) {
+            counts[c - 'a']--;
+            if (counts[c - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
