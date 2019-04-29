74a0040a4de540245aa2f587409735dfd7fc7e5c
vibrioh <vibrioh@gmail.com>
2019-04-29 10:59:30 -0400

    expressionaddoperators

--- Problems/src/fb/ExpressionAddOperators.java
+++ Problems/src/fb/ExpressionAddOperators.java
@@ -4,6 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ExpressionAddOperators {
+
+
+    // "+" only to do expression
+    public List<String> addOnly(String strNum, int target) {
+        List<String> res = new ArrayList<>();
+        dfsAdd(strNum, target, 0, "", 0, res);
+        return res;
+    }
+
+    private void dfsAdd(String strNum, int target, int start, String exp, long sum, List<String> res) {
+        if (start == strNum.length()) {
+            if (sum == target) {
+                res.add(exp);
+            }
+            return;
+        }
+        for (int i = start; i < strNum.length(); i++) {
+            long num = Long.parseLong(strNum.substring(start, i + 1));
+            //check strt == 0 is very important, it make sure start clean, get rid of i = 0 .. 1 .. 2 .. caused nums
+            if (start == 0) {
+                dfsAdd(strNum, target, i + 1, "" + num, num, res);
+            } else {
+                dfsAdd(strNum, target, i + 1, exp + "+" + num, sum + num, res);
+            }
+            if (num == 0) {
+                break;
+            }
+        }
+    }
+
+
+    // The whole: "+" "-" "*"
     private void dfs(String num, int target, int start, String str, long sum, long lastF, List<String> ans) {
         // used all string chars
         if (start == num.length()) {
@@ -36,5 +68,16 @@ public class ExpressionAddOperators {
         List<String> ans = new ArrayList<>();
         dfs(num, target, 0, "", 0, 0, ans);
         return ans;
+    }
+
+    public static void main(String[] args) {
+        ExpressionAddOperators e = new ExpressionAddOperators();
+        String[] strs = new String[]{"123", "232", "00", "01237"};
+        int[] targets = new int[]{6, 8, 0, 31};
+        for (int i = 0; i < 4; i++) {
+            System.out.println(e.addOnly(strs[i], targets[i]));
+            System.out.println(e.addOperators(strs[i], targets[i]));
+            System.out.println();
+        }
     }
 }
