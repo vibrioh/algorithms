a9aa389b2e930fea5bd07ecdbd607371040e96c4
vibrioh <vibrioh@gmail.com>
2019-05-04 23:05:07 -0400

    4 pointers

---
+++ Problems/src/ggl/ExpressiveWords.java
@@ -0,0 +1,14 @@
+package ggl;
+
+public class ExpressiveWords {
+    public boolean check(String S, String W) {
+        int n = S.length(), m = W.length(), i = 0, j = 0;
+        for (int i2 = 0, j2 = 0; i < n && j < m; i = i2, j = j2) {
+            if (S.charAt(i) != W.charAt(j)) return false;
+            while (i2 < n && S.charAt(i2) == S.charAt(i)) i2++;
+            while (j2 < m && W.charAt(j2) == W.charAt(j)) j2++;
+            if (i2 - i != j2 - j && i2 - i < Math.max(3, j2 - j)) return false;
+        }
+        return i == n && j == m;
+    }
+}
